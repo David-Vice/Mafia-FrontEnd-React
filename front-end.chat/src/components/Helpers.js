ef998132b4c5d8e0f9817bc8d14e09f4aec5cca3
Mame_ym86@itstep.edu.az <Mame_ym86@itstep.edu.az>
2022-05-25 14:39:55 +0400

    room creation from front-end.chat added

---
+++ front-end.chat/src/components/Helpers.js
@@ -0,0 +1,28 @@
+export function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin === -1) {
+        begin = dc.indexOf(prefix);
+        if (begin !== 0) return null;
+    }
+    else {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end === -1) {
+            end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+}
+export function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
--- front-end.chat/src/components/Lobby.js
+++ front-end.chat/src/components/Lobby.js
@@ -1,21 +1,62 @@
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { getCookie, parseJwt } from './Helpers'
+import Axios from 'axios'
 
 const Lobby = ({ joinRoom }) => {
-    const [user, setUser] = useState();
+    const [username, setUsername] = useState();
+    const [userid, setUserid] = useState();
     const [room, setRoom] = useState();
+    const [url, setUrl] = useState();
+    const [data, setData] = useState({
+        sessionName: "",
+        adminId: ""
+    });
 
-    return <Form className='lobby'
-        onSubmit={e => {
-            e.preventDefault();
-            joinRoom(user, room);
-        }} >
-        <Form.Group>
-            <Form.Control placeholder="name" onChange={e => setUser(e.target.value)} />
-            <Form.Control placeholder="room" onChange={e => setRoom(e.target.value)} />
-        </Form.Group>
-        <Button variant="success" type="submit" disabled={!user || !room}>Join</Button>
-    </Form>
+    function handleSubmit(e) {
+        e.preventDefault();
+        // console.log();
+        // setUserid(parseJwt(getCookie('token')).Id);
+        // setUsername(parseJwt(getCookie('token')).UserName)
+        setUrl("https://localhost:44313/api/Sessions");
+        // joinRoom(user, room);
+
+        console.log(room + "  " + parseJwt(getCookie('token')).Id);
+        console.log(room + "  " + userid);
+        console.log(room + "  " + parseJwt(getCookie('token')).Id);
+
+        Axios.post(url, {
+            sessionName: room,
+            adminId: parseJwt(getCookie('token')).Id
+        })
+            .then(res => {
+                console.log(res);
+            })
+            .catch((error) => {
+                if (error.response !== null) {
+                    console.log(error)
+                }
+            });
+
+    }
+
+
+    // setUser(parseJwt(getCookie('token')).UserName);
+    // console.log(parseJwt(getCookie('token')).Id);
+    return (
+        // <div className='home-container'>
+        <Form className='lobby'
+            onSubmit={handleSubmit}
+        >
+            <Form.Group>
+                {/* <Form.Control placeholder="name" onChange={e => setUser(parseJwt(getCookie('token')).UserName)} /> */}
+                <Form.Control placeholder="Room name" onChange={e => setRoom(e.target.value)} />
+            </Form.Group>
+            <Button variant="success" type="submit" disabled={!room}>Create room</Button>
+        </Form>
+        // </div>
+
+    )
 }
 
 export default Lobby;
